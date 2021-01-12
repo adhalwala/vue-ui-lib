8415a787cf4a50ed7e14365ceacfd87a2d9fb687
Abrar Dhalwala <adhalwala@aecordigital.com>
2021-01-12 15:34:37 +0530

    first commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import { VueUiLibSample } from '@/entry';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueUiLibSample,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-ui-lib-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as VueUiLibSample } from './vue-ui-lib-sample.vue';
